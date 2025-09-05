fb47e270d16210f8b99db714530016f10a7db81b
shahzaddmajid12 <shahzadmajid121@outlook.com>
2025-09-05 11:12:07 +0500

    Code

--- pythonCourse/Lecture03/main.py
+++ pythonCourse/Lecture03/main.py
@@ -2,32 +2,32 @@ print("*"*8)
 
 print("Sign Up")
 
-userName= input("Enter Your Name ")
+userName= input("Enter Your Name: ")
 
-userEmail= input("Enter Your Email ")
+userEmail= input("Enter Your Email: ")
 
-userPass= input("Enter Your  Password ")
+userPass= input("Enter Your  Password: ")
 
-userContact= input("Enter Your Contact ")
+userContact= input("Enter Your Contact: ")
 
 print("Account Successfully created")
 
-userEmailLogin= input("Enter Your Email ")
-userPassLogin= input("Enter Your  Password ")
+userEmailLogin= input("Enter Your Email: ")
+userPassLogin= input("Enter Your  Password: ")
 
 
 if userEmail==userEmailLogin and userPass== userPassLogin:
     print("Welcome In MITI ",userName )
-
+    print("*"*25)
     print("Program 01 Marksheet ")
-    eng=float(input("Enter Your English Marks"))
-    urdu=float(input("Enter Your Urdu Marks"))
-    math=float(input("Enter Your Math Marks"))
+    eng=float(input("Enter Your English Marks: "))
+    urdu=float(input("Enter Your Urdu Marks: "))
+    math=float(input("Enter Your Math Marks: "))
 
     obtained=eng+urdu+math
     meriPercemtage=obtained/300*100
-    print("Obatined Marks ", obtained)
-    print("Percentage ", meriPercemtage)
+    print("Obatined Marks = ", obtained)
+    print("Percentage = ", meriPercemtage, "%")
     if meriPercemtage<=100 and meriPercemtage>=80:
         print("Grade A1 MashaAllah ")
     elif meriPercemtage<=79 and meriPercemtage>=70:
@@ -43,31 +43,32 @@ if userEmail==userEmailLogin and userPass== userPassLogin:
     else:
         print("Try Again")
 
-    
+    print("*"*25)
     print("Program No 02 EVEN / ODD")
-
-    num=int(input("Enter Your Number "))
+    num=int(input("Enter Your Number: "))
     if num%2==0:
-        print("Even ",num)
+        print(num, "is Even ")
     else:
-        print("Odd", num)
+        print(num, "is Odd")
+    
+    print("*"*25)
     print("Program No 03 LEAP YEAR ")
-
-    year=int(input("Enter Your Year "))
+    year=int(input("Enter Any Year "))
     if year%4==0:
-        print("Leap Year ", year)
+        print("Leap Year: ", year)
     else:
-        print("Not a Leap Year",year)
+        print(year, "is not a Leap Year")
 
 
+    print("*"*25)
     print("Program No 04 POS/ NEG")
-    num1=int(input("Enter Your number "))
+    num1=int(input("Enter any number: "))
     if num1 <0:
-        print("negative")
+        print(num1, " is negative")
     else:
-        print("positive",num1)
+        print(num1, " is positive")
 
-    print("Positive") if num1<0 else print("Negative")
+    print("Positive") if num1>0 else print("Negative")
 
 
 else:
--- pythonCourse/Lecture03/shortHandifExample.py
+++ pythonCourse/Lecture03/shortHandifExample.py
@@ -1,3 +1,10 @@
-num1=int(input("Enter your number"))
-status="Positive" if num1>0 else "Negative"
+num1=int(input("Enter your number: "))
+# print("Positive") if num1 > 0 else print("Negative")
+# OR remove repititon by good practice
+status="Positive" if num1 > 0 else "Negative"
+print(status)
+
+
+ride = (input("Enter your ride status: "))
+status="On the way" if ride == 'started' else "Accepted" if ride == 'acc' else "Cancel" if ride == 'can' else "Requested"
 print(status)
