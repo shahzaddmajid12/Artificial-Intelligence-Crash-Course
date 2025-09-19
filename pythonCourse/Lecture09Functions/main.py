97c1c71f4020dce108ec9204ee55674145039895
shahzaddmajid12 <shahzadmajid121@outlook.com>
2025-09-19 15:12:45 +0500

    Code

--- pythonCourse/Lecture09Functions/main.py
+++ pythonCourse/Lecture09Functions/main.py
@@ -1,7 +1,8 @@
+# Function is Dont Repeat Yourself - DRY
 # This function 'username' takes one argument 'name' and prints it as the student's name
 def username(name):
     # Printing the student's name
-    print("Student Name:", name)
+    print("Student Name: ", name)
 
 # Calling the 'username' function with different names as arguments
 username("ali")  # Output: Student Name: ali
@@ -10,7 +11,7 @@ username("usman")  # Output: Student Name: usman
 # This function 'add' takes two arguments 'a' and 'b', and prints their sum
 def add(a, b):
     # Printing the sum of the two numbers
-    print(a + b)
+    print("adding a + b: ", a + b)
 
 # Calling the 'add' function with different pairs of numbers
 add(1, 22)  # Output: 23
@@ -18,24 +19,15 @@ add(112, 22)  # Output: 134
 add(133, 22)  # Output: 155
 add(1456, 22)  # Output: 1478
 
+# for practice create function based marksheet and calculator
 
 def square(number):
-    return number * number
+    print("square: ", number * number) 
 
 # Test the function
-print(square(5))  # Output: 25
-print(square(10))  # Output: 100
+square(5)  # Output: 25
+square(10)  # Output: 100
 
-
-def is_even_or_odd(number):
-    if number % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
-
-# Test the function
-print(is_even_or_odd(4))  # Output: Even
-print(is_even_or_odd(7))  # Output: Odd
 
 
 def find_max(a, b):
